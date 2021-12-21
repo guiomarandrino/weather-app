570fbaea018343cbb617e43a24336638f53c5c65
Guiomar Andrino <94846443+guiomarandrino@users.noreply.github.com>
2021-12-21 20:08:44 +0100

    Added files

---
+++ src/script.js
@@ -0,0 +1,64 @@
+let now = new Date();
+function formatDate(Date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDay = days[now.getDay()];
+  let currentHour = now.getHours();
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let currentDate = document.querySelector("#current-date");
+  currentDate.innerHTML = `${currentDay} ${currentHour}:${minutes}`;
+}
+formatDate();
+
+function displayWeather(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#current-temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+
+  document.querySelector("#weather-description").innerHTML =
+    response.data.weather[0].description;
+
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+}
+
+function searchCity(city) {
+  let apiKey = "d0b1e479fc5064851c4adfb88cebfb69";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+let cityForm = document.querySelector("#search-form");
+cityForm.addEventListener("submit", handleSubmit);
+
+function searchLocation(position) {
+  let apiKey = "d0b1e479fc5064851c4adfb88cebfb69";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
