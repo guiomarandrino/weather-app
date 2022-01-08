61feb773799703406b9ba1330683736bfc932541
Guiomar Andrino <guiomarandrinoes@gmail.com>
2022-01-09 00:04:42 +0100

    Forecast with data from API

--- src/script.js
+++ src/script.js
@@ -20,25 +20,51 @@ function formatDate(Date) {
 }
 formatDate();
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDate();
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row align-items-center">`;
-  let days = ["Thursday", "Friday", "Saturday", "Sunday", "Monday"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
         <div class="col-2">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather">☁</div>
+            <div class="weather-forecast-date">${formatDay(
+              forecastDay.dt
+            )}</div>
+            <img src="https://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png" alt="" width="39">
             <div class="weather-forecast-temperatures">
-             <span class="weather-forecast-temperature-max">6º |</span>
-             <span class="weather-forecast-temperature-min"> -1º</span>
+             <span class="weather-forecast-temperature-max">${Math.round(
+               forecastDay.temp.max
+             )}º |</span>
+             <span class="weather-forecast-temperature-min"> ${Math.round(
+               forecastDay.temp.min
+             )}º</span>
             </div>
         </div>
         `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
